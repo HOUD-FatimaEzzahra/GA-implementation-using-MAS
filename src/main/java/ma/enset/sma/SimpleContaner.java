82fa3d716348bab886e125a01f895fa4524d67d7
HOUD-FatimaEzzahra <fatimaezzahrahoud14@gmail.com>
2023-05-31 00:15:47 +0100

    docs

--- src/main/java/ma/enset/sma/SimpleContaner.java
+++ src/main/java/ma/enset/sma/SimpleContaner.java
@@ -4,6 +4,7 @@ import jade.core.Profile;
 import jade.core.ProfileImpl;
 import jade.core.Runtime;
 import jade.wrapper.AgentContainer;
+import jade.wrapper.AgentController;
 import jade.wrapper.ControllerException;
 import jade.wrapper.StaleProxyException;
 import ma.enset.GAUtils;
@@ -15,10 +16,10 @@ public class SimpleContaner {
         profile.setParameter(Profile.MAIN_HOST,"localhost");
         AgentContainer agentContainer=runtime.createAgentContainer(profile);
         for (int i = 0; i< GAUtils.ISLAND_NUMBER; i++){
-            agentContainer.createNewAgent("IslandAgent"+i,IslandAgent.class.getName(),new Object[]{});
-            agentContainer.start();
+            AgentController islandAgent= agentContainer.createNewAgent("IslandAgent"+i,IslandAgent.class.getName(),new Object[]{});
+            islandAgent.start();
         }
-        agentContainer.createNewAgent("MasterAgent",MasterAgent.class.getName(),new Object[]{});
-        agentContainer.start();
+        AgentController masterAgent = agentContainer.createNewAgent("MasterAgent",MasterAgent.class.getName(),new Object[]{});
+        masterAgent.start();
     }
 }
